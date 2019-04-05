143f9e52526ca113c266544bf21dfabf921bcfb4
David Wang <davidw1339@gmail.com>
2019-04-05 01:45:50 -0500

    changed to be simpler

--- code/image_changer.js
+++ code/image_changer.js
@@ -1,5 +1,4 @@
 let new_image = '' // place the url of the new image you want here
 
-// add an onload to define a function that runs when the current tab has loaded
-// in the function final all the images on the current page
+// find all the images on the current page
 // then loop through them and change all their src's
