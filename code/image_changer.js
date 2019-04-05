80350c845d0aaca60c34c1134ab209d91d23d989
David Wang <davidw1339@gmail.com>
2019-04-05 00:42:47 -0500

    changed src to variable

--- code/image_changer.js
+++ code/image_changer.js
@@ -1,10 +1,12 @@
+const new_image = 'https://pbs.twimg.com/profile_images/1065873857024798720/HWc2dqgb_400x400.jpg'
+
 // add an onload to define a function that runs when the current tab has loaded
 window.onload = function() {
     // find all the images on the current page
     let images = document.querySelectorAll('img');
     for (let image of images) { // loop through the images
         // change their src
-        image.src = 'https://pbs.twimg.com/profile_images/1065873857024798720/HWc2dqgb_400x400.jpg'
+        image.src = new_image
     }
 }
 
