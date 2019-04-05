97874c60acf7a0eef0901d17a998613c6e019c14
David Wang <davidw1339@gmail.com>
2019-04-05 00:53:18 -0500

    make the code unfinished

--- code/image_changer.js
+++ code/image_changer.js
@@ -1,13 +1,5 @@
-const new_image = 'https://pbs.twimg.com/profile_images/1065873857024798720/HWc2dqgb_400x400.jpg'
+const new_image = '' // place the url of the new image you want here
 
 // add an onload to define a function that runs when the current tab has loaded
-window.onload = function() {
-    // find all the images on the current page
-    let images = document.querySelectorAll('img');
-    for (let image of images) { // loop through the images
-        // change their src
-        image.src = new_image
-    }
-}
-
-
+// in the function final all the images on the current page
+// then loop through them and change all their src's
