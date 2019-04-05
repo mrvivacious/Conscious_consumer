3a14182ca5dea9678dae2a9d188d94a0cc28ab53
David Wang <davidw1339@gmail.com>
2019-04-05 00:56:43 -0500

    changed const to let

--- code/image_changer.js
+++ code/image_changer.js
@@ -1,4 +1,4 @@
-const new_image = '' // place the url of the new image you want here
+let new_image = '' // place the url of the new image you want here
 
 // add an onload to define a function that runs when the current tab has loaded
 // in the function final all the images on the current page
