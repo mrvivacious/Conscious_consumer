206e4556941fbabdbe48ad6e4810636c8b544ecf
David Wang <davidw1339@gmail.com>
2019-04-05 00:41:27 -0500

    added code for image changer

---
+++ code/image_changer.js
@@ -0,0 +1,11 @@
+// add an onload to define a function that runs when the current tab has loaded
+window.onload = function() {
+    // find all the images on the current page
+    let images = document.querySelectorAll('img');
+    for (let image of images) { // loop through the images
+        // change their src
+        image.src = 'https://pbs.twimg.com/profile_images/1065873857024798720/HWc2dqgb_400x400.jpg'
+    }
+}
+
+
